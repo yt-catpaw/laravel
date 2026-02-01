70da77ecfa195d1407f473259d8ea950a82898fc
tajimayuki <softte2su77@gmail.com>
2026-02-02 01:33:12 +0900

    phpstanエラー無視

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,6 +11,7 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
+        /** @phpstan-ignore-next-line */
         $this->assertTrue(true);
     }
 }
